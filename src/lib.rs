fb625fc3b9a11b5d7986301d81640c8828dfc014
Mohammad Aadil Shabier <64919407+aadilshabier@users.noreply.github.com>
2021-03-29 16:36:52 +0530

    Added testfiles and initialized whitespace funtion

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
-pub fn whitespace(filename: &str) {
-    println!("{}", filename);
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+pub fn whitespace(path: &str) -> String {
+    let mut output = String::new();
+    let f = File::open(path).expect(format!("File {} not found", path).as_str());
+
+    output
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_whitespace() {
-        unimplemented!();
+        let mut f1 = File::open("testfiles/input/a.txt").unwrap();
+        let mut f2 = File::open("testfiles/output/a.txt").unwrap();
+
+        let mut buffer1 = String::new();
+        let mut buffer2 = String::new();
+
+        f1.read_to_string(&mut buffer1).unwrap();
+        f2.read_to_string(&mut buffer2).unwrap();
+
+        assert_eq!(buffer1, buffer2);
     }
 }
