10d963a841a89057ab1c429553bf2c5b48a3725e
Mohammad Aadil Shabier <64919407+aadilshabier@users.noreply.github.com>
2021-03-29 15:49:20 +0530

    Added modularity and started project

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub fn whitespace(filename: &str) {
+    println!("{}", filename);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate clap;
+mod lib;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("formatter-rs")
+        .version("1.0")
+        .author("Mohammad Aadil Shabier, aadilshabier1@gmail.com")
+        .about("Simple rust file formatter")
+        .subcommand(
+            SubCommand::with_name("whitespace")
+                .about("Removes unwanted whitespace")
+                .version("1.0")
+                .arg(
+                    Arg::with_name("file")
+                        .long("file")
+                        .short("f")
+                        .help("Removes unwanted whitespace")
+                        .takes_value(true),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("whitespace") {
+        lib::whitespace(matches.value_of("file").unwrap());
+    }
 }
